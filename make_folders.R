4834924522ad41c0ccd05ed0a90f8abee6e43f65
Rob J Hyndman <robjhyndman@gmail.com>
2022-08-31 19:47:15 +0200

    Updated code to clean up hyndsight posts

--- make_folders.R
+++ make_folders.R
@@ -29,7 +29,8 @@ move_files <- function(folder, extension) {
 
 move_files("seminars", "md")
 move_files("publications", "md")
-# move_files("hyndsight", "md")
+move_files("hyndsight", "md")
+move_files("hyndsight", "qmd")
 
 remove_yaml_field <- function(file_contents, field) {
   # Find yaml
@@ -59,31 +60,26 @@ clean_yaml <- function(folder) {
   )
   for (i in seq_along(files)) {
     oldcontents <- contents <- read_lines(files[i])
-    # Was the md file generated from Rmd?
-    if (any(str_detect(contents, "^output:"))) {
-      fs::file_delete(files[i])
-    } else {
-      # Find yaml
-      yaml <- which(str_detect(contents, "^---"))
-      # Remove blank links from yaml
-      if (length(yaml) > 0) {
-        blank <- which(str_length(contents) == 0L)
-        if (length(blank) > 0) {
-          blank <- blank[blank < max(yaml)]
-        }
-        if (length(blank) > 0) {
-          contents <- contents[-blank]
-        }
+    # Find yaml
+    yaml <- which(str_detect(contents, "^---"))
+    # Remove blank links from yaml
+    if (length(yaml) > 0) {
+      blank <- which(str_length(contents) == 0L)
+      if (length(blank) > 0) {
+        blank <- blank[blank < max(yaml)]
       }
-      # Remove wordpress id, author and comments
-      contents <- remove_yaml_field(contents, "wordpress")
-      contents <- remove_yaml_field(contents, "author")
-      contents <- remove_yaml_field(contents, "comments")
-      contents <- remove_yaml_field(contents, "mathjax")
-      # Write file
-      if(!identical(contents, oldcontents)) {
-        write_lines(contents, files[i])
+      if (length(blank) > 0) {
+        contents <- contents[-blank]
       }
+    }
+    # Remove wordpress id, author and comments
+    contents <- remove_yaml_field(contents, "wordpress")
+    contents <- remove_yaml_field(contents, "author")
+    contents <- remove_yaml_field(contents, "comments")
+    contents <- remove_yaml_field(contents, "mathjax")
+    # Write file
+    if(!identical(contents, oldcontents)) {
+      write_lines(contents, files[i])
     }
   }
 }
