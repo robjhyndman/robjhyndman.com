8ff6a7593ff85f4b445097adc7995d2230e239a8
Rob J Hyndman <robjhyndman@gmail.com>
2022-09-17 12:31:24 +0200

    Added academicons extension

---
+++ _extensions/schochastics/academicons/academicons.lua
@@ -0,0 +1,67 @@
+-- function ensureLatexDeps()
+--   quarto.doc.useLatexPackage("academicons")
+-- end
+
+local function ensureHtmlDeps()
+  quarto.doc.addHtmlDependency({
+    name = "academicons",
+    version = "1.9.2",
+    stylesheets = {"assets/css/all.css", "assets/css/size.css"}
+  })
+end
+
+local function isEmpty(s)
+  return s == nil or s == ''
+end
+
+local function isValidSize(size)
+  local validSizes = {
+    "tiny", "scriptsize", "footnotesize", "small", "normalsize",
+    "large", "Large", "LARGE", "huge", "Huge",
+    "1x", "2x", "3x", "4x", "5x", "6x", "7x", "8x", "9x", "10x",
+    "2xs", "xs", "sm", "lg", "xl", "2xl"
+  }
+  for _, v in ipairs(validSizes) do
+    if v == size then
+      return " ai-" .. size
+    end
+  end
+  return ""
+end
+
+return {
+  ["ai"] = function(args, kwargs)
+
+    local group = ""
+    local icon = pandoc.utils.stringify(args[1])
+    if #args > 1 then
+      group = icon
+      icon = pandoc.utils.stringify(args[2])
+    end
+
+    local size = isValidSize(pandoc.utils.stringify(kwargs["size"]))
+
+    -- detect html (excluding epub)
+    if quarto.doc.isFormat("html:js") then
+      ensureHtmlDeps()
+      if isEmpty(size) then
+        return pandoc.RawInline(
+          'html',
+          "<i class=\"ai " .. group .. " ai-" .. icon .. "\"></i>"
+        )
+      else
+        return pandoc.RawInline(
+          'html',
+          "<i class=\"ai " .. group .. " ai-" .. icon .. size .. "\"></i>"
+        )
+      end
+    -- detect pdf / beamer / latex / etc
+    -- elseif quarto.doc.isFormat("pdf") then
+    --   ensureLatexDeps()
+    --   return pandoc.RawInline('tex', "\\aiIcon{" .. icon .. "}") 
+    else
+      return pandoc.Null()
+    end
+
+  end
+}
